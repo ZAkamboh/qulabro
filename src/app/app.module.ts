c19bf498476f5e90a900564aeba338e4311b9ea8
SMTaha <s.m.taha10@gmail.com>
2018-08-01 17:50:52 +0500

    chore(signup): add signup and related components
    add signup, verify, details and invite components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { IonicModule, Input } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { VerifyComponent } from './signup/verify/verify.component';
+import { DetailsComponent } from './signup/details/details.component';
+import { InviteComponent } from './signup/invite/invite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    VerifyComponent,
+    DetailsComponent,
+    InviteComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, IonicModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
