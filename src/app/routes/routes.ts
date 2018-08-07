8cc334098fd4c8db6e9db9f3a723468ee591820e
SMTaha <s.m.taha10@gmail.com>
2018-08-07 17:11:14 +0500

    feat(routes): add application routing definitions

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,29 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignupComponent } from '../signup/signup.component';
 
-const ROUTES_ARRAY = [
-  {
-    path: '/signup',
-    component: SignupComponent
-  },
-  {
-    path: '/login',
-    component: ''
-  },
-  {
-    path: '/',
-    redirectTo: '/signup'
-  },
-  {
-    path: '**',
-    redirectTo: '/signup'
-  }
-];
+import { SignupComponent } from '../signup/signup.component';
+import { EmailComponent } from '../signup/email/email.component';
+import { VerifyComponent } from '../signup/verify/verify.component';
+import { DetailsComponent } from '../signup/details/details.component';
+import { InviteComponent } from '../signup/invite/invite.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/signup', pathMatch: 'full' }, // Replace redirect to the actual component for this route
-  { path: 'signup', component: SignupComponent },
+  {
+    path: '',
+    redirectTo: '/signup',
+    pathMatch: 'full'
+  }, // Replace redirect to the actual component for this route
+  {
+    path: 'signup',
+    component: SignupComponent,
+    children: [
+      { path: 'verify', component: VerifyComponent },
+      { path: 'details', component: DetailsComponent },
+      { path: 'invite', component: InviteComponent },
+      { path: '', component: EmailComponent }
+    ]
+  },
   { path: 'login', redirectTo: '/signup' }, // Replace redirect to the actual component for this route
   { path: '**', redirectTo: '/signup' } // Replace redirect to the actual component for this route
 ];
