f2b590b534e2361ae101a902cc3c4648645e7bd6
unknown <adnannice@gmail.com>
2018-08-08 16:25:01 +0500

    feat: add styles to onboarding process screens

--- src/app/onboarding-body/onboarding-body.component.ts
+++ src/app/onboarding-body/onboarding-body.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-onboarding-body',
   templateUrl: './onboarding-body.component.html',
-  styleUrls: ['./onboarding-body.component.scss']
+  styleUrls: ['./onboarding-body.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class OnboardingBodyComponent implements OnInit {
 
